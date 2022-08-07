632b0683cc0956f8c85abf5d2f55a57193da7e71
Zhich <nick.zhicheng@gmail.com>
2022-08-07 00:31:50 -0400

    Initial Commit

---
+++ iConvert/iConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertApp.swift
+//  iConvert
+//
+//  Created by Nick on 2022-08-07.
+//
+
+import SwiftUI
+
+@main
+struct iConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
